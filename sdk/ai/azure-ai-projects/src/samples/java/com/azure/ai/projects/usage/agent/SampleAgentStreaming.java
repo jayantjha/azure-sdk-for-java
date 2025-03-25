5e09ea724fe6235e7db34644beb8f8bdbbd628c9
Jayant Kumar <kumarjayant@microsoft.com>
2025-03-25 15:30:34 +0530

    Streaming addition

--- sdk/ai/azure-ai-projects/src/samples/java/com/azure/ai/projects/usage/agent/SampleAgentStreaming.java
+++ sdk/ai/azure-ai-projects/src/samples/java/com/azure/ai/projects/usage/agent/SampleAgentStreaming.java
@@ -1,4 +1,96 @@
 package com.azure.ai.projects.usage.agent;
 
+import com.azure.ai.projects.AIProjectClientBuilder;
+import com.azure.ai.projects.AgentsClient;
+import com.azure.ai.projects.models.*;
+import com.azure.core.util.Configuration;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class SampleAgentStreaming {
+
+    @Test
+    void agentStreamingExample() {
+        AgentsClient agentsClient
+            = new AIProjectClientBuilder().endpoint(Configuration.getGlobalConfiguration().get("ENDPOINT", "endpoint"))
+            .subscriptionId(Configuration.getGlobalConfiguration().get("SUBSCRIPTIONID", "subscriptionid"))
+            .resourceGroupName(Configuration.getGlobalConfiguration().get("RESOURCEGROUPNAME", "resourcegroupname"))
+            .projectName(Configuration.getGlobalConfiguration().get("PROJECTNAME", "projectname"))
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildAgentsClient();
+
+        var agentName = "agent_streaming_example";
+        var createAgentOptions = new CreateAgentOptions("gpt-4o-mini")
+            .setName(agentName)
+            .setInstructions("You politely help with math questions. Use the code interpreter tool when asked to visualize numbers.")
+            .setTools(List.of(new CodeInterpreterToolDefinition()));
+        Agent agent = agentsClient.createAgent(createAgentOptions);
+
+        var thread = agentsClient.createThread();
+        var createdMessage = agentsClient.createMessage(
+            thread.getId(),
+            MessageRole.USER,
+            "Hi, Assistant! Draw a graph for a line with a slope of 4 and y-intercept of 9.");
+
+
+
+
+
+
+
+
+
+
+
+
+        // ---------------------- usual code --------------------
+
+        //run agent
+        var createRunOptions = new CreateRunOptions(thread.getId(), agent.getId())
+            .setAdditionalInstructions("");
+        var threadRun = agentsClient.createRun(createRunOptions);
+
+        try {
+            do {
+                Thread.sleep(500);
+                threadRun = agentsClient.getRun(thread.getId(), threadRun.getId());
+            }
+            while (
+                threadRun.getStatus() == RunStatus.QUEUED
+                    || threadRun.getStatus() == RunStatus.IN_PROGRESS
+                    || threadRun.getStatus() == RunStatus.REQUIRES_ACTION);
+
+            if (threadRun.getStatus() == RunStatus.FAILED) {
+                System.out.println(threadRun.getLastError().getMessage());
+            }
+
+            var runMessages = agentsClient.listMessages(thread.getId());
+            for (ThreadMessage message : runMessages.getData())
+            {
+                System.out.print(String.format("%1$s - %2$s : ", message.getCreatedAt(), message.getRole()));
+                for (MessageContent contentItem : message.getContent())
+                {
+                    if (contentItem instanceof MessageTextContent)
+                    {
+                        System.out.print((((MessageTextContent) contentItem).getText().getValue()));
+                    }
+                    else if (contentItem instanceof MessageImageFileContent)
+                    {
+                        String imageFileId = (((MessageImageFileContent) contentItem).getImageFile().getFileId());
+                        System.out.print("Image from ID: " + imageFileId);
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            //cleanup
+            agentsClient.deleteThread(thread.getId());
+            agentsClient.deleteAgent(agent.getId());
+        }
+    }
 }
