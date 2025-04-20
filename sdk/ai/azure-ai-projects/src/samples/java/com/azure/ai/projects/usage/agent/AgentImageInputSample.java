b49706eabc11a4ad645f9fc41062117ca458cf99
Jayant Kumar <kumarjayant@microsoft.com>
2025-04-20 23:26:26 +0530

    fabric and image input

---
+++ sdk/ai/azure-ai-projects/src/samples/java/com/azure/ai/projects/usage/agent/AgentImageInputSample.java
@@ -0,0 +1,121 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.ai.projects.usage.agent;
+
+import com.azure.ai.projects.AIProjectClientBuilder;
+import com.azure.ai.projects.AgentsClient;
+import com.azure.ai.projects.models.Agent;
+import com.azure.ai.projects.models.AgentThread;
+import com.azure.ai.projects.models.CreateAgentOptions;
+import com.azure.ai.projects.models.CreateRunOptions;
+import com.azure.ai.projects.models.MessageContent;
+import com.azure.ai.projects.models.MessageImageFileContent;
+import com.azure.ai.projects.models.MessageImageUrlParam;
+import com.azure.ai.projects.models.MessageInputContentBlock;
+import com.azure.ai.projects.models.MessageInputImageUrlBlock;
+import com.azure.ai.projects.models.MessageInputTextBlock;
+import com.azure.ai.projects.models.MessageRole;
+import com.azure.ai.projects.models.MessageTextContent;
+import com.azure.ai.projects.models.OpenAIPageableListOfThreadMessage;
+import com.azure.ai.projects.models.RunStatus;
+import com.azure.ai.projects.models.ThreadMessage;
+import com.azure.ai.projects.models.ThreadRun;
+import com.azure.core.util.BinaryData;
+import com.azure.core.util.Configuration;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+
+public final class AgentImageInputSample {
+
+    @Test
+    void imageInputBase64Example() throws IOException, URISyntaxException {
+        AgentsClient agentsClient
+            = new AIProjectClientBuilder().endpoint(Configuration.getGlobalConfiguration().get("ENDPOINT", "endpoint"))
+            .subscriptionId(Configuration.getGlobalConfiguration().get("SUBSCRIPTIONID", "subscriptionid"))
+            .resourceGroupName(Configuration.getGlobalConfiguration().get("RESOURCEGROUPNAME", "resourcegroupname"))
+            .projectName(Configuration.getGlobalConfiguration().get("PROJECTNAME", "projectname"))
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildAgentsClient();
+
+
+        Path file = getFile("sample_image.jpg");
+        byte[] imageContent = Files.readAllBytes(file);
+        String imageBase64 = Base64.getEncoder().encodeToString(imageContent);
+        String imageUrl = "data:image/png;base64," + imageBase64;
+
+        List<MessageInputContentBlock> messageBlock = Arrays.asList(new MessageInputTextBlock("Hello, what is in the image"),
+            new MessageInputImageUrlBlock(new MessageImageUrlParam(imageUrl)));
+
+
+        String agentName = "image_input_example";
+        CreateAgentOptions createAgentOptions = new CreateAgentOptions("gpt-4o")
+            .setName(agentName)
+            .setInstructions("You are a helpful agent");
+        Agent agent = agentsClient.createAgent(createAgentOptions);
+
+        AgentThread thread = agentsClient.createThread();
+        ThreadMessage createdMessage = agentsClient.createMessage(
+            thread.getId(),
+            MessageRole.USER,
+            BinaryData.fromObject(messageBlock));
+
+        //run agent
+        CreateRunOptions createRunOptions = new CreateRunOptions(thread.getId(), agent.getId())
+            .setAdditionalInstructions("");
+        ThreadRun threadRun = agentsClient.createRun(createRunOptions);
+
+        try {
+            do {
+                Thread.sleep(500);
+                threadRun = agentsClient.getRun(thread.getId(), threadRun.getId());
+            }
+            while (
+                threadRun.getStatus() == RunStatus.QUEUED
+                    || threadRun.getStatus() == RunStatus.IN_PROGRESS
+                    || threadRun.getStatus() == RunStatus.REQUIRES_ACTION);
+
+            if (threadRun.getStatus() == RunStatus.FAILED) {
+                System.out.println(threadRun.getLastError().getMessage());
+            }
+
+            OpenAIPageableListOfThreadMessage runMessages = agentsClient.listMessages(thread.getId());
+            for (ThreadMessage message : runMessages.getData()) {
+                System.out.print(String.format("%1$s - %2$s : ", message.getCreatedAt(), message.getRole()));
+                for (MessageContent contentItem : message.getContent()) {
+                    if (contentItem instanceof MessageTextContent) {
+                        System.out.print((((MessageTextContent) contentItem).getText().getValue()));
+                    } else if (contentItem instanceof MessageImageFileContent) {
+                        String imageFileId = (((MessageImageFileContent) contentItem).getImageFile().getFileId());
+                        System.out.print("Image from ID: " + imageFileId);
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //cleanup
+            agentsClient.deleteThread(thread.getId());
+            agentsClient.deleteAgent(agent.getId());
+        }
+    }
+
+    private Path getFile(String fileName) throws FileNotFoundException, URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new FileNotFoundException("File not found");
+        }
+        File file = new File(resource.toURI());
+        return file.toPath();
+    }
+}
